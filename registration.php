ab6d69adb614f05261eeb724bc8e6065e5826d0a
mekarseo <mekarseo@hotmail.com>
2016-06-20 14:30:50 +0700

    update-20.6.1016

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Magento2 Thai Language Pack (th_TH)
+ * 
+ * Github: https://github.com/jucksearm/magento2-language-th-th
+ * 
+ * @author Jucksearm Boonmor <jucksearm.bkk@gmail.com>
+ * @copyright Copyright © 2016 Jucksearm
+ * @version 100.6.11
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+        'jucksearm_th_th',
+        __DIR__
+);
